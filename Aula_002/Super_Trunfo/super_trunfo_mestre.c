0963084cb4f447ffbfbc2d8681768c784b00fff6
MiguelDecotelli <migueldecotelli@icloud.com>
2025-03-19 12:06:52 -0300

    "Super Trunfo Países - desafio nível mestre"

--- Aula_002/Super_Trunfo/super_trunfo_mestre.c
+++ Aula_002/Super_Trunfo/super_trunfo_mestre.c
@@ -3,14 +3,14 @@
 
 int main()
 {
-    // Declaração das variáveis das Carta 1 e 2 - Desafio Novato.
+    // Declaração das variáveis das Cartas 1 e 2 para os Países - Desafio Mestre.
     char pais1[50], pais2[50];
     unsigned int populacao1, populacao2;
     float area_pais1, area_pais2;
     float PIB1, PIB2;
     int numero_pontos_turisticos1, numero_pontos_turisticos2;
-    int trunfo1, trunfo2, trunfo3;
-    char *turno1, *turno2, *turno3;
+    int trunfo1, trunfo2, trunfo3, trunfo4;
+    char *turno1, *turno2, *turno3, *turno4;
     int contador = 0;
 
     printf("======================SUPER TRUNFO: PAÍSES======================\n");
@@ -47,7 +47,7 @@ int main()
     printf("Informe o número de pontos turísticos do País 2: ");
     scanf("%d", &numero_pontos_turisticos2);
 
-    // Cálculo das variáveis - Desafio Intermediário.
+    // Cálculo das variáveis - Desafio Mestre.
     float densidade_populacional1 = (float)populacao1 / area_pais1;
     float PIB_per_capita1 = (PIB1 * 10000000) / (float)populacao1;
 
@@ -62,6 +62,7 @@ int main()
     float super_poder1 = ((float)populacao1 + area_pais1 + PIB1 + numero_pontos_turisticos1 + PIB_per_capita1 + densidade_populacional_invertida1);
     float super_poder2 = ((float)populacao2 + area_pais2 + PIB2 + numero_pontos_turisticos2 + PIB_per_capita2 + densidade_populacional_invertida2);
 
+    // Escolha do atributo no jogo - Desafio Mestre
     printf("\nAgora escolha um atributo: \n");
     printf("1. População\n");
     printf("2. Área do País\n");
@@ -72,6 +73,7 @@ int main()
     printf("7. Super Trunfo\n");
     scanf("%d", &trunfo1);
 
+    // Primeiro switch para definição do vencedor na rodada 01 - Desafio mestre.
     switch (trunfo1)
     {
     case 1:
@@ -114,6 +116,7 @@ int main()
         break;
     }
 
+    // Escolha do segundo atributo no jogo - Desafio Mestre
     printf("\nAgora escolha um novo atributo: \n");
     printf("1. População\n");
     printf("2. Área do País\n");
@@ -124,8 +127,10 @@ int main()
     printf("7. Super Trunfo\n");
     scanf("%d", &trunfo2);
 
+    // Verificar se o atributo selecionado é diferente do anterior.
     if (trunfo1 != trunfo2)
     {
+        // Segundo switch para definição do vencedor na rodada 02 - Desafio mestre.
         switch (trunfo2)
         {
         case 1:
@@ -173,6 +178,7 @@ int main()
         printf("Você já escolheu esse trunfo antes!");
     }
 
+    // Escolha do terceiro atributo no jogo - Desafio Mestre
     printf("\nAgora escolha um novo atributo: \n");
     printf("1. População\n");
     printf("2. Área do País\n");
@@ -183,12 +189,14 @@ int main()
     printf("7. Super Trunfo\n");
     scanf("%d", &trunfo3);
 
+    // Verificar se o atributo selecionado é diferente dos anteriores.
     if (trunfo1 != trunfo2 && trunfo1 != trunfo3 && trunfo2 != trunfo3)
     {
+        // Terceiro switch para definição do vencedor na rodada 04 - Desafio mestre.
         switch (trunfo3)
         {
         case 1:
-            // Comparando População
+            // Comparando a população
             turno3 = populacao1 > populacao2 ? (contador++, "A população do País do jogador é maior que a do País do computador\n") : "A população do País do computador é maior que a população do País do jogador\n";
             printf("%s", turno3);
             break;
@@ -232,9 +240,76 @@ int main()
         printf("Você já escolheu esse trunfo antes\n!");
     }
 
-    if (contador >= 2)
+    // Escolha do quarto atributo no jogo - Desafio Mestre
+    printf("\nAgora escolha um novo atributo: \n");
+    printf("1. População\n");
+    printf("2. Área do País\n");
+    printf("3. PIB\n");
+    printf("4. Pontos turísticos\n");
+    printf("5. Densidade populacional\n");
+    printf("6. PIB per capita\n");
+    printf("7. Super Trunfo\n");
+    scanf("%d", &trunfo4);
+
+    // Verificar se o atributo selecionado é diferente dos anteriores.
+    if (trunfo1 != trunfo2 && trunfo1 != trunfo3 && trunfo2 != trunfo3 && trunfo1 != trunfo4 && trunfo2 != trunfo4 && trunfo3 != trunfo4)
+    {
+        // Quarto switch para definição do vencedor na rodada 04 - Desafio mestre.
+        switch (trunfo4)
+        {
+        case 1:
+            // Comparando a população
+            turno4 = populacao1 > populacao2 ? (contador++, "A população do País do jogador é maior que a do País do computador\n") : "A população do País do computador é maior que a população do País do jogador\n";
+            printf("%s", turno4);
+            break;
+        case 2:
+            // Comparando a área do pais
+            turno4 = area_pais1 > area_pais2 ? (contador++, "A área do País do jogador é maior que a área do País do computador\n") : "A área do País do computador é maior que a área do País do jogador\n";
+            printf("%s", turno4);
+            break;
+        case 3:
+            // Comparando o PIB do pais
+            turno4 = PIB1 > PIB2 ? (contador++, "O PIB do País do jogador é maior que o PIB do País do computador\n") : "O PIB do País do computador é maior que o PIB do País do jogador\n";
+            printf("%s", turno4);
+            break;
+        case 4:
+            // Comparando o número de pontos turísticos
+            turno4 = numero_pontos_turisticos1 > numero_pontos_turisticos2 ? (contador++, "O número de pontos turísticos do País do jogador é maior que o número de pontos turísticos do País do computador\n") : "O número de pontos turísticos do País do computador é maior que o número de pontos turísticos do País do jogador\n";
+            printf("%s", turno4);
+            break;
+        case 5:
+            // Comparando a densidade populacional
+            turno4 = densidade_populacional1 > densidade_populacional2 ? (contador++, "A densidade populacional do País do jogador é maior que a densidade populacional do País do computador\n") : "A densidade populacional do País do computador é maior que a densidade populacional do País do jogador\n";
+            printf("%s", turno4);
+            break;
+        case 6:
+            // Comparando o PIB per capita
+            turno4 = PIB_per_capita1 > PIB_per_capita2 ? (contador++, "O PIB per capita do País do jogador é maior que o PIB per capita do País do computador\n") : "O PIB per capita do País do computador é maior que o PIB per capita do País do jogador\n";
+            printf("%s", turno4);
+            break;
+        case 7:
+            // Comparando o super poder
+            turno4 = super_poder1 > super_poder2 ? (contador++, "O super poder do jogador é maior que o super poder do computador\n") : "O super poder do computador é maior que o super poder do jogador\n";
+            printf("%s", turno4);
+            break;
+        default:
+            printf("OPÇÃO INVÁLIDA!\n");
+            break;
+        }
+    }
+    else
+    {
+        printf("Você já escolheu esse trunfo antes\n!");
+    }
+
+    // Utilizando um contador, é possível verificar o número de rodadas vencidas pelo jogador e pelo computador.
+    if (contador > 2)
     {
         printf("Parabéns jogador, Voce venceu %d rodadas!\n", contador);
+    }
+    else if (contador == 2)
+    {
+        printf("Jogador, Você e o computador empataram!!\n");
     }
     else if (contador == 1)
     {
